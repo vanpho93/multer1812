f139a36460f6d8766aa9184cc41be248cedac2f4
Van Pho Nguyen <vanpho02@gmail.com>
2018-01-19 20:42:59 +0700

    limits

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 const express = require('express');
 const reload = require('reload');
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => cb(null, './public'),
+    filename: (req, file, cb) => cb(null, Date.now() + '.png')
+});
+
+const fileFilter = (req, file, cb) => {
+    if (file.mimetype !== 'image/png' && file.mimetype !== 'image/jpeg') {
+        // return cb(null, false);
+        return cb(new Error('File type error.'))
+    }
+    cb(null, true);
+}
+
+const upload = multer({ storage, fileFilter, limits: { fileSize: 50 * 1024 } });
 
 const app = express();
 app.set('view engine', 'ejs');
@@ -10,7 +26,9 @@ app.get('/upload', (req, res) => {
     res.render('upload');
 });
 
-app.post('/upload', (req, res) => {
+app.post('/upload', upload.single('avatar'), (req, res) => {
+    console.log(req.body);
+    console.log(req.file);
     res.send('Thanh cong');
 });
 
