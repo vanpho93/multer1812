e618d0aff1c32413183eb0663e1888eb0d06c8d9
Van Pho Nguyen <vanpho02@gmail.com>
2018-01-19 21:14:28 +0700

    config

--- index.js
+++ index.js
@@ -11,10 +11,36 @@ app.get('/upload', (req, res) => {
     res.render('upload');
 });
 
-app.post('/upload', upload.single('avatar'), (req, res) => {
-    console.log(req.body);
-    console.log(req.file);
-    res.send('Thanh cong');
+app.post('/upload', (req, res) => {
+    upload.single('avatar')(req, res, err => {
+        if (err) return res.send(err.message);
+        console.log(req.body);
+        console.log(req.file);
+        res.send('Thanh cong');
+    });
+});
+
+app.post('/uploadArray', (req, res) => {
+    upload.array('hinhSanPham')(req, res, err => {
+        if (err) return res.send(err.message);
+        console.log(req.body);
+        console.log(req.files.map(f => f.filename));
+        res.send('Thanh cong');
+    });
+});
+
+app.post('/uploadFields', (req, res) => {
+    const fieldsConfig = [
+        { name: 'hinhChinh', maxCount: 1 },
+        { name: 'hinhPhu', maxCount: 3 }
+    ];
+    upload.fields(fieldsConfig)(req, res, err => {
+        if (err) return res.send(err.message);
+        console.log(req.body);
+        console.log(req.files);
+        // console.log(req.files.map(f => ({ name: f.filename, role: f.fieldName })));
+        res.send('Thanh cong');
+    });
 });
 
 app.listen(3000, () => console.log('Server started!'));
